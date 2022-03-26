35eb0795bc41cfe6b9a99e1fef90496ee63e48f1
Jubaer Alnazi <97327489+h33tlit@users.noreply.github.com>
2022-03-27 02:26:04 +0800

    Create tool.py

---
+++ tool.py
@@ -0,0 +1,101 @@
+import json
+import requests
+from concurrent.futures import ThreadPoolExecutor
+import re
+from fake_useragent import UserAgent
+
+
+#Author: Jubaer Alnazi
+
+
+
+fetched_url = []
+common_fetched_url = set()
+ua = UserAgent()
+domain = input('Type domain: ')
+
+
+print("=>>> We just started! Give us some time!")
+try:
+    alienvault_request_fetch = requests.get('https://otx.alienvault.com/api/v1/indicators/hostname/'+domain+'/url_list?limit=1000', timeout=5, headers = {'User-Agent':str(ua.chrome)}).json()
+    for request_url in alienvault_request_fetch['url_list']:
+        for characters in request_url['url']:
+            if '?' and '=' in characters:
+                common_fetched_url.add(request_url['url'])
+            else:
+                pass
+except:
+    pass
+
+waybackURL = "https://web.archive.org/cdx/search/cdx?url=*."+ domain +"&output=json&fl=original&collapse=urlkey"
+
+
+try:
+    request = requests.get(waybackURL,headers = {'User-Agent':str(ua.chrome)}, timeout=5)
+    load = json.loads(request.text)
+    for ur in load:
+        for char in ur[0]:
+            if '?' and '=' in char:
+                common_fetched_url.add(ur[0])
+            else:
+                pass
+except:
+    pass
+
+
+for url in common_fetched_url:
+    try:
+        urlpara = url.split("?")[1]
+        urlpara2 = urlpara.split("&")
+        for parameters in urlpara2:
+                para_index = parameters.split("=")
+                para_string = str(para_index[0] + "=" + para_index[1])
+                param_check = str(para_index[0]) + "=" + str(para_index[1]).replace(str(para_index[1]), 'jUbAeR')
+                formatted_url = url.replace(para_string, param_check)
+                fetched_url.append(formatted_url)
+    except:
+        pass
+
+print("==>>> We will be scanning "+str(len(fetched_url))+" links!")
+
+
+
+def check_xss(xss_urls):
+    try:
+        req = requests.get(xss_urls, timeout=1, headers = {'User-Agent':str(ua.chrome)}).text
+        regex = re.findall('jUbAeR', req)
+        if len(regex) != 0:
+            return str(xss_urls)
+        else:
+            pass
+    except:
+        pass
+
+
+found_links = []
+open_redirect = []
+try:
+
+    with ThreadPoolExecutor(max_workers=1000) as pool:
+        response_list = list(pool.map(check_xss, fetched_url))
+    file_write = []
+    for r in response_list:
+        if r is not None:
+            found_links.append(r)
+            if 'url' and '=http' in r:
+                open_redirect.append(r)
+except:
+    pass
+
+if len(found_links) != 0:
+    print('\n#######################-  Possible XSS   -###########################')
+    print(found_links)
+    print('\n#######################-  Possible Open Redirect   -###########################')
+    if len(open_redirect) != 0:
+        print(open_redirect)
+    elif len(open_redirect) == 0:
+        print("No links found!")
+elif len(found_links) == 0:
+    print('\n#######################-  Result   -###########################')
+    print('We could not find anything :( ')
+
